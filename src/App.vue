3bc55475412f592b5d5a4e9bdff3134797ef22ab
Jett Drafahl <jettdrafahl@gmail.com>
2025-04-21 16:38:29 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+
+
+<template> 
+   <div class="text-center text-2xl mt-10">
+    NBA Purgatory — it's about to get real 🏀🔥
+  </div>
+</template>
+
+
+
+<script setup>
+//empty for now
+</script>
+
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
